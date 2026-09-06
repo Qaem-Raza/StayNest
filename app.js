c63b46a77c997358b195046d3503d915f0929fdb
Qaem Raza <qaemraza110@gmail.com>
2026-09-06 18:58:48 +0530

    Second commit, i created the Update Route and Delete Route.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./Models/listings.js");
+const methodOverride = require("method-override");
 
 MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -20,6 +21,7 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/" , (req,res) => {
     res.send("Hi, I am at root");
@@ -50,6 +52,28 @@ app.post("/listings",async (req,res) =>{
     res.redirect("/listings");
 });
 
+//Edit Route
+app.get("/listings/:id/edit", async (req,res) =>{
+
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+// Update Route
+app.put("/listings/:id", async (req,res) =>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+// Delete Route
+app.delete("/listings/:id", async (req,res) =>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 
 // app.get("/testlisting",async (req,res) => {
 //     let sampleListing = new Listing({
